6120f11b9acd4660ef0e80c3c704bbe729cf51bc
mbedoya <mauricio.bedoya@gmail.com>
2016-11-10 18:41:49 -0500

    registration id

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-  .run(function ($ionicPlatform) {
+  .run(function ($ionicPlatform, $rootScope) {
     $ionicPlatform.ready(function () {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
@@ -61,6 +61,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
           var handle = data.registrationId;
 
           alert(handle);
+          $rootScope.registrationID = handle;
 
           // Set the device-specific message template.
           if (platform == 'android' || platform == 'Android') {
